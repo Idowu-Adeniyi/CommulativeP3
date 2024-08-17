ab014f76d3728e17717ee77571fcbf359bde266f
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2024-08-16 23:38:06 -0400

    recent files

---
+++ ComulativePart1/Scripts/teachers.js
@@ -0,0 +1,49 @@
+﻿// AJAX for Teacher Add can go in here!
+// This file is connected to the project via Shared/_Layout.cshtml
+
+
+function AddTeacher() {
+
+	//goal: send a request which looks like this:
+	//POST : http://localhost:44390/api/TeacherData/AddTeacher
+	//with POST data of teachername, hiredate, employeenumber, etc.
+
+	var URL = "http://localhost:44390/api/TeacherData/AddTeacher/";
+
+	var rq = new XMLHttpRequest();
+	// where is this request sent to?
+	// is the method GET or POST?
+	// what should we do with the response?
+
+	var TeacherFname = document.getElementById('TeacherFname').value;
+	var TeacherLname = document.getElementById('TeacherLname').value;
+	var EmployeeNumber = document.getElementById('EmployeeNumber').value;
+	var Salary = document.getElementById('Salary').value;
+
+	
+
+	var TeacherData = {
+		"TeacherFname": TeacherFname,
+		"TeacherLname": TeacherLname,
+		"EmployeeNumber": EmployeeNumber,
+		"Salary": Salary
+	};
+
+
+	rq.open("POST", URL, true);
+	rq.setRequestHeader("Content-Type", "application/json");
+	rq.onreadystatechange = function () {
+		//ready state should be 4 AND status should be 200
+		if (rq.readyState == 4 && rq.status == 200) {
+			//request is successful and the request is finished
+
+			//nothing to render, the method returns nothing.
+
+			
+		}
+
+	}
+	//POST information sent through the .send() method
+	rq.send(JSON.stringify(TeacerData));
+
+}
